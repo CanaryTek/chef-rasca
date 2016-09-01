37de956246a7262b7578ef226957239633d704b0
Miguel Armas <kuko@canarytek.com>
2016-09-01 14:16:40 +0100

    Change rasca gem version

--- attributes/rasca.rb
+++ attributes/rasca.rb
@@ -8,7 +8,7 @@ default['nagios_name']=Chef::Config[:node_name]
 
 # Gem repo and version to install
 default['rasca']['gemrepo']="http://gems.canarytek.com/"
-default['rasca']['gem_version']="0.1.30"
+default['rasca']['gem_version']="0.1.31"
 
 # Packages
 case node['platform_family']
